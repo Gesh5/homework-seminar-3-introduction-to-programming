353cb9dc0aae91af87fb1ee10c7bf81099298545
Gesh5 <129482161+Gesh5@users.noreply.github.com>
2023-04-24 19:12:06 +1000

    Add files via upload

---
+++ семинар 3/task 001/Program.cs
@@ -0,0 +1,66 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+
+// Решение 1
+
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int len = number.Length;
+
+// if (len == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - НЕ палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"{number} - не является пятизначным");
+// }
+
+
+// решение 2
+
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000)
+{
+    int a1 = number / 10000;
+    int b1 = number % 10;
+    if (a1 == b1)
+    {
+        number = number / 10;
+        int a2 = number % 10;
+        int b2 = (number / 100) % 10;
+
+        if (a2 == b2)
+
+            Console.WriteLine("палиндромом");
+    }
+
+    else
+    {
+        Console.WriteLine("Не палиндромом");
+    }
+
+
+}
+else
+{
+    Console.WriteLine("Не пятизначное число!");
+}
---
+++ семинар 3/task 002/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Расстояние между двумя точками в трехмерном пространстве
+// (Хb – Xa)^2 + (Yb – Ya)^2 + (Zb – Za)^2
+
+Console.WriteLine("введите Xa ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Ya ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Za ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Xb ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Yb ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Zb ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write(Math.Round(Math.Sqrt(Math.Pow((X2-X1),2) + Math.Pow((Y2-Y1),2)+ Math.Pow((Z2-Z1),2)) ,2));
+Console.Write (" - растояние между точками а и b в 3D пространстве");
